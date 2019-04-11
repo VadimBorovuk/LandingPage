55db7b3a35c0924965153cec8752b40f45610de2
VadimBorovuk <borowik.vadim@gmail.com>
2019-04-11 11:26:54 +0300

    validation

--- js/common.js
+++ js/common.js
@@ -18,21 +18,48 @@ allLink.forEach(function (elem) {
 });
 
 
+//Validation
+
+var btn = document.querySelector('#btn');
+
+
+
+btn.addEventListener('click',function (e){
+
+    var name = document.getElementById('name')
+    var email = document.querySelector('#email');
+    var pass = document.querySelector('#pass');
+
+    if(name.value.length < 3){
+        name.style.border = '3px solid red'
+    }else{
+        name.style.border = "1px solid #fff"
+    }
+
+    if(pass.value.length < 8){
+        pass.style.border = '3px solid red'
+    }else{
+        pass.style.border = "1px solid #fff"
+    }
+
+})
+
+
 //For Ajax
-// var subBtn = document.querySelector('#btn');
-//
-// subBtn.addEventListener('click', function (e) {
-//     e.preventDefault()
-//     var xhr = new XMLHttpRequest();
-//
-//     xhr.onreadystatechange = function () {
-//         if (xhr.readyState == 4 && xhr.status == 200) {
-//             alert(xhr.responseText)
-//         }
-//     };
-//
-//     xhr.open('GET', 'index.json');
-//     xhr.send();
-// })
-//
+var subBtn = document.querySelector('.subscribe__btn');
+
+subBtn.addEventListener('click', function (e) {
+    e.preventDefault()
+    var xhr = new XMLHttpRequest();
+
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            alert(xhr.responseText)
+        }
+    };
+
+    xhr.open('GET', 'index.json');
+    xhr.send();
+})
+
 
